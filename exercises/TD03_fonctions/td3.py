e6e078dc600f103b17d66b04f7835b91588add37
Paul-Arthur MARCEAUX <paul-arthur.nom@ens.uvsq.fr>
2023-09-29 11:05:18 +0200

    TD3 fini et TD 4 en cours

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -146,6 +146,7 @@ def tempsEnDate(temps):
 
 
 def afficheDate(date):
+    #print(date)
     print(date[0],"années",date[1],"mois",date[2],"jour",date[3],"heure",date[4],"min",date[5],"sec")
 
     
@@ -153,3 +154,105 @@ temps = secondeEnTemps(1000000000)
 afficheTemps(temps)
 print(tempsEnDate(temps))
 afficheDate(tempsEnDate(temps))
+
+
+def bisextile(jour):
+    compteur_annee_bisextile=1970
+    annee_bisextile=[]
+
+    while jour>0:
+
+        
+        #print(compteur_annee_bisextile-1968)
+        #print(compteur_annee_bisextile)
+
+        if ((compteur_annee_bisextile-1968)%4==0):  
+
+            if ((compteur_annee_bisextile)%100==0 and (compteur_annee_bisextile)%400!=0 ):
+                
+                jour-=365
+
+            else: 
+                annee_bisextile.append(compteur_annee_bisextile)
+                jour-=366
+        else:
+
+            jour-=365
+
+        compteur_annee_bisextile+=1
+
+    return annee_bisextile
+print(bisextile(20000))
+
+
+def nombreBisextile(jour):
+   return len( bisextile(jour))
+
+
+def tempsEnDateBisextile(temps):
+
+    date_0=(1970,0,1,0,0,0)
+    
+    new_temps=temps[0]-nombreBisextile(temps[0])*366
+
+    date= new_temps//365,(new_temps%365)//31,new_temps%365,temps[1],temps[2],temps[3]
+
+
+    
+    a=date[0]+date_0[0]
+    m=date[1]+date_0[1]
+    j=date[2]+date_0[2]
+    h=date[3]+date_0[3]
+    min=date[4]+date_0[4]
+    s=date[5]+date_0[5]
+
+    if s>60:
+        h+=1
+        s=s%60
+
+    if min>60:
+        h+=1
+        min=min%60
+
+    if h>24:
+        j+=1
+        h=h%24
+
+    if j>31:
+        m+=1
+        j=j%31
+
+    if m>12:
+        a+=1
+        m=m%12
+
+
+    date=a,m,j,h,min,s
+    return date
+    
+
+
+
+
+
+temps = secondeEnTemps(1000000000)
+afficheTemps(temps)
+afficheDate(tempsEnDateBisextile(temps))
+afficheDate(tempsEnDate(temps))
+
+
+def verifie(liste_temps):
+
+    seconde_des_semaines=0
+    for i in range(len(liste_temps)):
+        if tempsEnSeconde(liste_temps[i])<48*3600:
+            print(f"semaine {i} ok")
+
+    for j in range(len(liste_temps)):
+        seconde_des_semaines+=tempsEnSeconde(liste_temps[j])
+    if seconde_des_semaines<140*3600:
+        print("mois ok")
+
+
+liste_temps = [[1,2,39,34],[0,1,9,4],[0,29,39,51],[0,31,13,46]]
+verifie(liste_temps)
---
+++ exercises/TD04_listes/syrac.py
@@ -0,0 +1,87 @@
+def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+
+    nb=[]
+    while n!=1:
+        if n%2==0:
+            nb.append(n)
+            n=n/2
+        else:
+            nb.append(n)
+            n=3*n+1
+    nb.append(n)
+    return nb
+
+print(syracuse(3))
+
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 1 à n_max """
+    conjecture=True
+    for i in range(1,n_max):
+        last_nb=syracuse(i)[-1]
+        if last_nb!=1:
+            conjecture=False
+    
+    return conjecture
+
+print(testeConjecture(10000))
+
+
+def tempsVol(n):
+    """ Retourne le temps de vol de n """
+    nb=[]
+    compteur=0
+    while n!=1:
+        if n%2==0:
+            nb.append(n)
+            n=n/2
+        else:
+            nb.append(n)
+            n=3*n+1
+        compteur+=1
+    nb.append(n)
+    return compteur
+
+print("Le temps de vol de", 3, "est", tempsVol(3))
+
+def tempsVolListe(n_max):
+    """ Retourne la liste de tous les temps de vol de 1 à n_max """
+    temps_de_vol_total=[]
+    for i in range(1,n_max):
+        temps_de_vol_total.append(tempsVol(i))
+    return temps_de_vol_total
+
+#print(tempsVolListe(100))
+
+def plus_grand_temps_de_vol(n_max):
+    a=tempsVolListe(n_max)
+    plus_grand_vol=max(a)
+    print(plus_grand_vol)
+    for i in range(len(a)):
+        if plus_grand_vol==a[i]:
+            indice_du_plus_grand=i+1
+    return indice_du_plus_grand
+
+
+print(plus_grand_temps_de_vol(10000))
+
+
+def plus_grand_nb(n_max):
+    plus_grand_nb_atteint=[]
+    for i in range(1,n_max):
+        
+        while i!=1:
+            if i>max(plus_grand_nb_atteint):
+                plus_grand_nb_atteint.append(i)
+
+            if i%2==0:
+               
+                i=i/2
+            else:
+
+                i=3*i+1
+    plus_grand=max(plus_grand_nb_atteint)
+
+    return plus_grand
+
+print(plus_grand_nb(10000))
